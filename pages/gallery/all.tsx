169d6e9ee96d20e9f1d53598601376f587e6924f
Jadom <matquadev@gmail.com>
2022-04-19 21:50:45 -0300

    Update Gallery styles

--- pages/gallery/all.tsx
+++ pages/gallery/all.tsx
@@ -13,6 +13,24 @@ import { useMediaQuery } from 'react-responsive';
 export default function all() {
     const {data, isLoading} = useFirestore('images');
     const isResponsive = useMediaQuery({ query: '(min-width: 1200px)' });
+    const [query, setQuery] = useState('');
+
+    const keys = ["title"];
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setQuery(e.target.value)
+    }
+    
+    const search = ()=>{
+        return(
+            data.filter(
+                (image: any)=> 
+                    image.title.includes(query) ||
+                    image.title.toLowerCase().includes(query) ||
+                    image.title.toUpperCase().includes(query) 
+            ) 
+        )
+    }
 
     return (
         <Layout title="Gallery - Snow">
@@ -22,13 +40,15 @@ export default function all() {
                 <div className={styles.searchContainer}>
                     <div className={styles.search}>
                         <span><AiOutlineSearch/></span>
-                        <input type="text" placeholder='Search images'/>
+                        <input type="text" placeholder='Search images' value={query} onChange={handleChange}/>
                     </div>
                 </div>
                     { isLoading ? <Loader/> : <div className={styles.masonry}>
-                        {data.map((col: any)=>(
+                        {!query ? data.map((col: any)=>(
                                 <GalleryImage img={col.url} key={col.id} user={col.user}/>
-                        ))}
+                        )) : search().map((col: any)=>(
+                            <GalleryImage img={col.url} key={col.id} user={col.user}/>
+                    ))}
                     </div>}
                 </AuthProvider>
             </div>

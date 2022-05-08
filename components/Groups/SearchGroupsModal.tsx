b7902c1647303760ac08c93fda2cbb473ee4c49f
Jadom <matquadev@gmail.com>
2022-05-08 11:15:43 -0300

    [add] New features added to groups

--- components/Groups/SearchGroupsModal.tsx
+++ components/Groups/SearchGroupsModal.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import styles from './SearchGroupModal.module.scss'
 
 export default function SearchGroupsModal() {
+  const [query, setQuery] = useState('');
+  const [allGroups, setAllGroups] = useState<any>([])
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+        const users = await axios.get('http://localhost:5000/api/users/get/all');
+        setAllGroups([...users.data]);
+    }
+    fetchData();
+  },[]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    setQuery(e.target.value);
+  }
+
+
   return (
-    <div>SearchGroupsModal</div>
+    <div className="searchGroupsModal">
+        <div className="modal">
+          <button className="close"></button>
+          <h3>Search Groups</h3>
+          <div className="inputSearch">
+            <span><AiOutlineSearch/></span>
+            <input 
+            type="text" 
+            placeholder="Search People" 
+            value={query} 
+            onChange={handleChange}
+           	/>
+          </div>
+          <div className="groups">
+            {
+              allGroups.map((group: any)=>(
+				<div className={styles.group}>
+					<div className={styles.content}>
+						<div className={styles.groupPic}>
+							<img src="" alt="" />
+						</div>
+						<div className={styles.text}>
+							<h5></h5>
+							<div>
+								<p></p>
+								<p></p>
+							</div>
+						</div>
+						<div className={styles.button}>
+							<button>Join</button>
+						</div>
+					</div>
+				</div>
+			  ))
+            }
+          </div>
+        </div>
+    </div>
   )
 }

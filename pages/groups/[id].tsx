5dc5b252c6d8df0b48a5e36789aea4f1c7411b06
Jadom <matquadev@gmail.com>
2022-06-27 19:37:42 -0300

    [update] Update meta tags

--- pages/groups/[id].tsx
+++ pages/groups/[id].tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import GroupComponent from '../../components/Groups/GroupComponent';
@@ -26,6 +27,24 @@ export default function Group() {
         <>
             {group && (
                 <Layout title={group.title + ' - Snow'}>
+                    <Head>
+                        <meta charSet="utf-8" className="next-head" />
+                        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <meta name="author" content="Mateo Leal" />
+                        <meta name="description" content="Find new groups in Snow!" />
+                        <meta name="og:title" content={`Visit ${group.title} group - Snow`} />
+                        <meta name="og:url" content={'https://snowcy.com/user/' + group.title} />
+                        <meta name="og:site_name" content="Snow" />
+                        <meta name="og:description" content="Find new groups in Snow!" />
+                        <meta
+                            name="keywords"
+                            content="Social, Network, Snow, Social Network, User Profile"
+                        />
+                        <meta property="og:image" content={group && group.groupPic} />
+                        <meta property="og:image:width" content="500" />
+                        <meta property="og:image:height" content="500" />
+                    </Head>
                     <AuthProvider>
                         <GroupComponent group={group} />
                     </AuthProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Head from 'next/head';
 import FriendList from '../components/Friends/FriendList';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 import PostList from '../components/Posts/PostList';
 import Suggestions from '../components/Suggestions/Suggestions';
 import { AuthProvider } from '../context/AuthContext';
@@ -12,26 +12,60 @@ import SlideFriends from '../components/Responsive/SlideFriends';
 import ResposiveToPost from '../components/Responsive/Posts/ResposiveToPost';
 
 const Home = () => {
-  const isResponsive = useMediaQuery({ query: '(min-width: 1200px)' });
-  return (
-    <>
-    <Layout title="Snow">
-      <div className={styles.homeContainer}>
-        <AuthProvider>
-          {isResponsive &&
-          <div className={styles.bar}>
-            <FriendList/>
-            <OptionsBar/>
-          </div> 
-          }
-          <PostList/>
-          {isResponsive && <Suggestions/>}
-          {!isResponsive && <ResposiveToPost/>}
-        </AuthProvider>
-      </div>
-    </Layout>
-    </>
-  )
-}
+    const isResponsive = useMediaQuery({ query: '(min-width: 1200px)' });
+    return (
+        <>
+            <Layout title="Snow">
+                <>
+                    <Head>
+                        <meta charSet="utf-8" className="next-head" />
+                        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <meta name="author" content="Mateo Leal" />
+                        <meta
+                            name="description"
+                            content="Snow is a new modern social network! Sign in to connect with your friends and find groups and more."
+                        />
+                        <meta name="og:title" content="Snow" />
+                        <meta name="og:url" content="https://snowcy.com" />
+                        <meta name="og:site_name" content="Snow" />
+                        <meta
+                            name="og:description"
+                            content="Snow is a new modern social network! Sign in to connect with your friends and find groups and more."
+                        />
+                        <meta name="keywords" content="Social, Network, Snow, Social Network" />
+                        <meta property="og:image" content="https://i.imgur.com/RjNqmPi.png" />
+                        <meta property="og:image:width" content="500" />
+                        <meta property="og:image:height" content="500" />
+                        <link
+                            rel="stylesheet"
+                            type="text/css"
+                            charSet="UTF-8"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                        />
+                        <link
+                            rel="stylesheet"
+                            type="text/css"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                        />
+                    </Head>
+                    <div className={styles.homeContainer}>
+                        <AuthProvider>
+                            {isResponsive && (
+                                <div className={styles.bar}>
+                                    <FriendList />
+                                    <OptionsBar />
+                                </div>
+                            )}
+                            <PostList />
+                            {isResponsive && <Suggestions />}
+                            {!isResponsive && <ResposiveToPost />}
+                        </AuthProvider>
+                    </div>
+                </>
+            </Layout>
+        </>
+    );
+};
 
-export default Home
+export default Home;

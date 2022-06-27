9bd2bbab13a303eb5100924279813d22325fc724
Jadom <matquadev@gmail.com>
2022-06-27 18:04:08 -0300

    [update] update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@
  */
 
 const nextConfig = {
-    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-        return {
-            '/': { page: '/' },
-            '/user': { page: '/user', query: { slug: 'username' } },
-            '/groups': { page: '/groups', query: { slug: 'id' } }
-        };
-    },
     reactStrictMode: true,
     trailingSlash: true
 };

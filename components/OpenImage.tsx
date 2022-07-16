0b5839f8e822942b6033138ec80e68188cb9d149
Jadom <matquadev@gmail.com>
2022-07-15 21:13:13 -0300

    [add] keyboard shortcut in image modal

--- components/OpenImage.tsx
+++ components/OpenImage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './OpenImage.module.scss';
 
 interface Props {
@@ -9,10 +9,20 @@ interface Props {
 
 export default function OpenImage({ img, openImage, setOpenImage }: Props) {
     const [isClose, setIsClose] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('keydown', (e: any) => {
+            if (e.keyCode === 27) {
+                setIsClose(true);
+            }
+        });
+    });
+
     const handleClick = () => {
         setTimeout(() => setOpenImage(false), 550);
         setIsClose(true);
     };
+
     return (
         <div
             className={`${styles.openImageContainer} ${isClose && styles.close}`}

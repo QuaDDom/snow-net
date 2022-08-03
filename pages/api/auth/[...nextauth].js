49b920f4a261a79c9a33c358ea80b43d1e3e49ee
Jadom <matquadev@gmail.com>
2022-08-03 19:44:23 -0300

    [update] configure next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,24 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/github';
 
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-        // ...add more providers here
-    ]
+    ],
+    jwt: {
+        encryption: true
+    },
+    secret: process.env.secret,
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === 'google') {
+                return profile.email_verified && profile.email.endsWith('@example.com');
+            }
+            return true; // Do different verification for other providers that don't have `email_verified`
+        }
+    }
 });

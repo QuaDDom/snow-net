0de242c105aa808c179a93c95d99f2ed7d14d8cc
Jadom <matquadev@gmail.com>
2022-09-14 18:46:59 -0300

    [fix] directory

--- components/Updates/NewUpdateFeatureList.tsx
+++ components/Updates/NewUpdateFeatureList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './NewUpdateFeatureList.scss';
+import styles from './NewUpdateFeatureList.module.scss';
 
 export default function NewUpdateFeatureList() {
     return <div>NewUpdateFeatureList</div>;
